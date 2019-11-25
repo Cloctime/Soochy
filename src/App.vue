1eaee5a002f92ad7143627f83351b5856e9f8782
Guillaume Montassier <guillaume.montassier@outlook.fr>
2019-11-25 17:11:57 +0100

    Header final
    Header terminé

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
   <div id="app">
+    <div id="header">
+      <img src="./assets/soochy120px.png" alt="logo Soochy">
+    </div>
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
@@ -8,13 +11,18 @@
   </div>
 </template>
 
-<style>
+<style scoped>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+
+#header {
+  height: 130px;
+  background-color: #CF2431;
+  padding: 0 20px;
+  text-align: left;
 }
 
 #nav {
